0205f855d2ac0de44aa5b44e75532eb5834b7261
tiffany lo <tiffany.mtlo@gmail.com>
2019-03-13 13:18:55 -0700

    added dynamic collection info to index page

--- static/js/index.js
+++ static/js/index.js
@@ -1,7 +1,11 @@
 $(function() {
-  $('.grid').masonry({
-    itemSelector: '.grid-item',
-    columnWidth: 90,
-    gutter: 3
+  $.get('/collections', function(response) {
+    response.forEach(function(collection, index) {
+      const $collectionInfoContainer = $('.collection-detail').find('[data-collection-id="' + collection.id + '"]');
+      const $photoNumEl = $collectionInfoContainer.find('.on-hover-layer__photos-num');
+      const $personNumEl = $collectionInfoContainer.find('.on-hover-layer__persons-num');
+      $photoNumEl.html(collection.numPhotos + ' photo(s)');
+      $personNumEl.html(collection.numPersons + ' person(s)');
+    });
   });
 });
